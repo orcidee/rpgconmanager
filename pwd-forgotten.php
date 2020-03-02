03fb7186078ba5e6736add22837bc1759e12d221
Cornut Matthieu <matthieu.cornut@liip.ch>
2020-03-02 16:44:09 +0100

    Fix password reset + add css

--- pwd-forgotten.php
+++ pwd-forgotten.php
@@ -13,83 +13,72 @@ $head = "
         <meta name='keywords' lang='fr' content='orcidee'>
 		<link rel='stylesheet' type='text/css' href='css/jquery-ui.css' />
         <link rel='stylesheet' type='text/css' href='css/styles.css' />
-		<link rel='stylesheet' type='text/css' href='css/2013.css' />
+		<link rel='stylesheet' type='text/css' href='css/2019.css' />
 		
     </head>
     <body>
 ";
 
 require_once(dirname(__FILE__).'/conf/conf.php');
-
-$dbServer = mysql_connect(HOST,USER,PASSWORD) or die("Impossible de se connecter : " . mysql_error());;
-$db = (mysql_select_db(DB));
-mysql_query("SET NAMES 'utf8'");
-
 require_once(dirname(__FILE__).'/classes/controls.php');
 require_once(dirname(__FILE__).'/classes/user.php');
 require_once(dirname(__FILE__).'/classes/orcimail.php');
 
-if(!$db){
-    echo "<p class='dbg'>Impossible de selectionner la base de donnees</p>";
-}else{
+echo $head;
 
+$user = User::getFromSession();
+if($user) {
+
+    // Vous êtes déjà authentifié
     echo $head;
-    
-    $user = User::getFromSession();
-    if($user) {
-        
-        // Vous êtes déjà authentifié
-        echo $head;
-        ?>
-        <h1>Authentification</h1>
-        <div class='login'>
-            <p>Tu es déjà authentifié</p>
-            <ul>
-                <li><a href="<?php echo Controls::home();?>?page=profile">Voir le profil</a></li>
-                <li><a href="<?php echo Controls::home();?>?page=logout">Se déconnecter</a></li>
-                <li><a href="<?php echo Controls::home();?>?page=list">Liste des parties</a></li>
-                <li><a href="<?php echo Controls::home();?>?page=create">Inscrire une nouvelle partie</a></li>
-            </ul>
-        </div>
-        <?php
-        
-    } else {
-        $msg = null;
-        if(!$user && isset($_POST['email'])){
-            $email = $_POST["email"];
-            if(Controls::validateEmail($email)){
-                if(User::emailExists($email)){
-                    $user = User::pseudoAuth($email);
-                    $pwd = $user->resetPassword();
-                    if($pwd){
-                        if(Orcimail::sendPassword($user, $pwd)){
-                            $msg = "Le mot de passe a été réinitialisé, vous le trouverez dans l'email qui vient de vois être envoyé.";
-                        }else{
-                            $msg = "Erreur inconnue lors de la réinitialisation du mot de passe.";
-                        }
+    ?>
+    <h1>Authentification</h1>
+    <div class='login'>
+        <p>Tu es déjà authentifié</p>
+        <ul>
+            <li><a href="<?php echo Controls::home();?>?page=profile">Voir le profil</a></li>
+            <li><a href="<?php echo Controls::home();?>?page=logout">Se déconnecter</a></li>
+            <li><a href="<?php echo Controls::home();?>?page=list">Liste des parties</a></li>
+            <li><a href="<?php echo Controls::home();?>?page=create">Inscrire une nouvelle partie</a></li>
+        </ul>
+    </div>
+    <?php
+
+} else {
+    $msg = null;
+    if(!$user && isset($_POST['email'])){
+        $email = $_POST["email"];
+        if(Controls::validateEmail($email)){
+            if(User::emailExists($email)){
+                $user = User::pseudoAuth($email);
+                $pwd = $user->resetPassword();
+                if($pwd){
+                    if(Orcimail::sendPassword($user, $pwd)){
+                        $msg = "Le mot de passe a été réinitialisé, vous le trouverez dans l'email qui vient de vous être envoyé.";
                     }else{
                         $msg = "Erreur inconnue lors de la réinitialisation du mot de passe.";
                     }
                 }else{
-                    $msg = "Cette adresse email nous est inconnue. Peut-être avez-vous utilisé une autre adresse par le passé. N'hésitez pas à vous enregistrer à nouveau si tel est votre désir.";
+                    $msg = "Erreur inconnue lors de la réinitialisation du mot de passe.";
                 }
             }else{
-                $msg = "Saisissez une adresse email valide SVP ! Ainsi nous serons en mesure de vous réinitialiser le mot de passe.";
+                $msg = "Cette adresse email nous est inconnue. Peut-être avez-vous utilisé une autre adresse par le passé. N'hésitez pas à vous enregistrer à nouveau si tel est votre désir.";
             }
-        }
-        ?><form action='' method='POST'>
-            <label for='email'>Email *</label>
-            <input type='text' name='email' value='' />
-            <input type='submit' value="Récupérer le mot de passe" class='submit' />
-        </form><?php
-        
-        if(!is_null($msg)){
-            echo "<p class='form-result'>$msg</p>";
-            echo "<p><a href='login.php'>Retour à la page d'authentification et enregistrement...</a></p>";
+        }else{
+            $msg = "Saisissez une adresse email valide SVP ! Ainsi nous serons en mesure de vous réinitialiser le mot de passe.";
         }
     }
+    ?><form action='' method='POST'>
+        <label for='email'>Email *</label>
+        <input type='text' name='email' value='' />
+        <input type='submit' value="Récupérer le mot de passe" class='submit' />
+    </form><?php
+
+    if(!is_null($msg)){
+        echo "<p class='form-result'>$msg</p>";
+        echo "<p><a href='login.php'>Retour à la page d'authentification et enregistrement...</a></p>";
+    }
 }
-mysql_close($dbServer);
 include("scripts.php");
 
 ?>
